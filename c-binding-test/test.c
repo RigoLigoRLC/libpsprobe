f5223587b99565c580ddac209c32135d154965b8
RigoLigo <rigoligo03@gmail.com>
2023-12-25 01:39:50 +0800

    New: Added family variant name getter

--- c-binding-test/test.c
+++ c-binding-test/test.c
@@ -8,6 +8,12 @@ typedef size_t lsprobe_status;
 lsprobe_status lsprobe_families_get(void **families, size_t *count);
 lsprobe_status lsprobe_families_get_name(void *family, size_t index,
                                          char **name, size_t *name_length);
+lsprobe_status lsprobe_families_get_variant_count(void *family, size_t index,
+                                                  size_t *variant_count);
+lsprobe_status lsprobe_families_get_variant_name(void *family, size_t index,
+                                                 size_t variant_index,
+                                                 char **variant_name,
+                                                 size_t *variant_name_length);
 lsprobe_status lsprobe_families_destroy(void *families);
 
 int main() {
@@ -20,7 +26,7 @@ int main() {
       "lsprobe_families_get: return %lx; families: %p, families_size: %ld;\n",
       status, families, families_size);
 
-  char *family_name_buffer = malloc(32);
+  char *name_buffer = malloc(32);
   size_t family_name_buffer_size = 32;
   for (size_t i = 0; i < families_size; i++) {
     char *family_name;
@@ -31,15 +37,37 @@ int main() {
            family_name, name_length);
 
     if (family_name_buffer_size < name_length + 1) {
-      family_name_buffer = realloc(family_name_buffer, name_length + 1);
+      name_buffer = realloc(name_buffer, name_length + 1);
       family_name_buffer_size = name_length + 1;
     }
-    memcpy(family_name_buffer, family_name, name_length);
-    family_name_buffer[name_length] = '\0';
-    printf("pretty name: %s;\n", family_name_buffer);
+    memcpy(name_buffer, family_name, name_length);
+    name_buffer[name_length] = '\0';
+    printf("pretty name: %s;\n", name_buffer);
+
+    // Get variants names
+    size_t variant_count;
+    status = lsprobe_families_get_variant_count(families, i, &variant_count);
+    printf("    lsprobe_families_get_variant_count: %lx; count: %ld;\n", status,
+           variant_count);
+    for (size_t j = 0; j < variant_count; j++) {
+      char *variant_name;
+      size_t variant_name_length;
+      status = lsprobe_families_get_variant_name(families, i, j, &variant_name,
+                                                 &variant_name_length);
+      printf(
+          "    lsprobe_families_get_variant_name: %lx; name: %p, length: %ld; ",
+          status, variant_name, variant_name_length);
+      if (family_name_buffer_size < variant_name_length + 1) {
+        name_buffer = realloc(name_buffer, variant_name_length + 1);
+        family_name_buffer_size = name_length + 1;
+      }
+      memcpy(name_buffer, variant_name, variant_name_length);
+      name_buffer[variant_name_length] = '\0';
+      printf("pretty name: %s;\n", name_buffer);
+    }
   }
 
-  free(family_name_buffer);
+  free(name_buffer);
 
   status = lsprobe_families_destroy(families);
   printf("lsprobe_families_destroy: %lx;\n", status);
---
+++ src/families.rs
@@ -0,0 +1,110 @@
+
+use probe_rs::config::ChipFamily;
+
+pub struct ExtDeviceFamilies {
+    families: Vec<ChipFamily>
+}
+
+#[no_mangle]
+pub extern "C" fn lsprobe_families_get(families_out: *mut *mut ExtDeviceFamilies, families_count_out: *mut usize) -> u32 {
+    match probe_rs::config::families() {
+        Ok(families) => {
+            let families_count = families.len();
+            let boxed_families = Box::new(ExtDeviceFamilies { families });
+
+            unsafe {
+                *families_count_out = families_count;
+                *families_out = Box::into_raw(boxed_families);
+            }
+
+            0
+        }
+
+        Err(_) => {
+            unsafe {
+                *families_count_out = 0;
+                *families_out = std::ptr::null_mut();
+            }
+
+            1
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn lsprobe_families_get_variant_count(families: *const ExtDeviceFamilies, index: usize, variant_count_out: *mut usize) -> u32 {
+    if families.is_null() {
+        return 1;
+    }
+
+    let families = unsafe { &* families };
+
+    if index >= families.families.len() {
+        return 1;
+    }
+
+    let family = &families.families[index];
+
+    unsafe {
+        *variant_count_out = family.variants.len();
+    }
+
+    0
+}
+
+#[no_mangle]
+pub extern "C" fn lsprobe_families_get_variant_name(families: *const ExtDeviceFamilies, index: usize, variant_index: usize, name_out: *mut *const u8, name_length_out: *mut usize) -> u32 {
+    if families.is_null() {
+        return 1;
+    }
+
+    let families = unsafe { &*families };
+
+    if index >= families.families.len() {
+        return 1;
+    }
+
+    let variant = &families.families[index].variants[variant_index];
+
+    unsafe {
+        *name_out = variant.name.as_ptr();
+        *name_length_out = variant.name.len();
+    }
+
+    0
+}
+
+#[no_mangle]
+pub extern "C" fn lsprobe_families_get_name(families: *const ExtDeviceFamilies, index: usize, name_out: *mut *const u8, name_length_out: *mut usize) -> u32 {
+    if families.is_null() {
+        return 1;
+    }
+
+    let families = unsafe { &*families };
+
+    if index >= families.families.len() {
+        return 1;
+    }
+
+    let family = &families.families[index];
+
+    unsafe {
+        *name_out = family.name.as_ptr();
+        *name_length_out = family.name.len();
+    }
+
+    0
+}
+
+#[no_mangle]
+pub extern "C" fn lsprobe_families_destroy(families: *mut ExtDeviceFamilies) -> u32 {
+    if families.is_null() {
+        return 1;
+    }
+
+    unsafe {
+        let _ = Box::from_raw(families);
+    }
+
+    0
+}

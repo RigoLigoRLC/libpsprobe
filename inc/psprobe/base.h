aad0b3794f9ce4e502a612d2191afbdff23bedfe
RigoLigo <rigoligo03@gmail.com>
2023-12-26 15:17:54 +0800

    New: Add standard C binding header

---
+++ inc/psprobe/base.h
@@ -0,0 +1,4 @@
+
+#pragma once
+
+typedef size_t psprobe_status;
---
+++ inc/psprobe/families.h
@@ -0,0 +1,29 @@
+
+/*
+    libpsprobe C API
+
+    families.h: probe-rs chip families resources getter
+*/
+
+#pragma once
+
+#include "base.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+psprobe_status psprobe_families_get(void **families, size_t *count);
+psprobe_status psprobe_families_get_name(void *family, size_t index,
+                                         char **name, size_t *name_length);
+psprobe_status psprobe_families_get_variant_count(void *family, size_t index,
+                                                  size_t *variant_count);
+psprobe_status psprobe_families_get_variant_name(void *family, size_t index,
+                                                 size_t variant_index,
+                                                 char **variant_name,
+                                                 size_t *variant_name_length);
+psprobe_status psprobe_families_destroy(void *families);
+
+#ifdef __cplusplus
+}
+#endif
